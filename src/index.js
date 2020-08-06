26eaf90d054226c5e2bfb81f6a1d466913dfb40d
Krithin Sitaram <krithin.sitaram@remix.com>
2020-08-06 04:39:40 -0700

    Hook up the text area to the aerial layer.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import mapboxgl from 'mapbox-gl';
 import Compare from 'mapbox-gl-compare';
@@ -9,7 +9,7 @@ import Compare from 'mapbox-gl-compare';
 // mapboxgl.accessToken = '<your access token here>';
 mapboxgl.accessToken = 'pk.eyJ1Ijoia3JpdGhpbiIsImEiOiJjamR6anRzOXUwZmdoMzJsanFyYmx6dDRoIn0.eHcws4Zzktx3d0iPN4Oumg';
 
-// tileguide_server_url = 'https://onetwotwo.sg';
+// const tileguide_server_url = 'https://onetwotwo.sg';
 const tileguide_server_url = 'http://localhost:8080';
 var addTileGuideLayers = (map) => {
   map.addSource('tileguide', {
@@ -30,57 +30,81 @@ var addTileGuideLayers = (map) => {
     'type': 'line',
     'source': 'tileguide',
     'source-layer': 'borders',
-  });
+  }, 'centers');
 };
 
-var addAerialRasterLayer = (map) => {
+var addAerialRasterLayer = (map, urlTemplate) => {
   map.addSource('raster-tiles', {
     type: 'raster',
-    tiles: ['https://www.portlandmaps.com/arcgis/rest/services/Public/Aerial_Photos_Summer_2018/MapServer/tile/{z}/{y}/{x}'],
+    tiles: [urlTemplate],
   });
   map.addLayer({
     id: 'raster-tiles',
     type: 'raster',
     source: 'raster-tiles',
-  });
+  }, 'borders');
+};
+
+const URLTemplateInput = (props) => {
+  const textArea = React.useRef();
+  return (
+    <div id='url-template-container' className='sidebarStyle'>
+      <label>URL Template:
+        <textarea ref={textArea} id='url-template' rows="1" defaultValue={props.value} onChange={() => props.onChange(textArea.current.value)} />
+      </label>
+    </div>
+  )
 };
 
 const Application = (props) => {
+  const [urlTemplate, setUrlTemplate] = useState('https://ecn.t0.tiles.virtualearth.net/tiles/a{quadkey}.jpeg?g=587&mkt=en-gb&n=z');
+
+  const baseMap = React.useRef(null);
+  const aerialMap = React.useRef(null);
   React.useEffect(() => {
-    const baseMap = new mapboxgl.Map({
+    baseMap.current = new mapboxgl.Map({
       container: 'basemap',
       style: 'mapbox://styles/mapbox/light-v10',
       center: [0, 0],
       zoom: 0
     });
-    const aerialMap = new mapboxgl.Map({
+    baseMap.current.on('load', () => {
+      addTileGuideLayers(baseMap.current);
+    });
+    aerialMap.current = new mapboxgl.Map({
       container: 'aerial',
       style: 'mapbox://styles/mapbox/dark-v10',
       center: [0, 0],
       zoom: 0
     });
-    baseMap.on('load', () => {
-      addTileGuideLayers(baseMap);
-    });
-    aerialMap.on('load', () => {
-      addAerialRasterLayer(aerialMap);
-      addTileGuideLayers(aerialMap);
+    aerialMap.current.on('load', () => {
+      addTileGuideLayers(aerialMap.current);
+      addAerialRasterLayer(aerialMap.current, urlTemplate);
     });
 
     // A selector or reference to HTML element
     var container = document.getElementById('comparison-container');
 
-    var compare = new Compare(baseMap, aerialMap, container, {
+    new Compare(baseMap.current, aerialMap.current, container, {
       orientation: 'vertical',
       mousemove: false,
-    })
+    });
   }, []);
+
+  React.useEffect(() => {
+    console.log(urlTemplate);
+    if (aerialMap.current.loaded()) {
+      if (aerialMap.current.getSource('raster-tiles')) {
+        aerialMap.current.removeLayer('raster-tiles');
+        aerialMap.current.removeSource('raster-tiles');
+      }
+      addAerialRasterLayer(aerialMap.current, urlTemplate);
+    }
+  }, [urlTemplate]);
 
   return (
     <div>
-      <div id='url-template-container' className='sidebarStyle'>
-        <label>URL Template:<input id='url-template' /></label>
-      </div>
+      <URLTemplateInput defaultValue={urlTemplate} onChange={setUrlTemplate} />
       <div id='comparison-container'>
         <div id='basemap' className='map'></div>
         <div id='aerial' className='map'></div>
